9ee4fbfcffad9b1a748c138f90cd1390462e90b0
Bo Wang <wangboo@amazon.com>
2015-07-27 21:47:58 -0700

    package change

--- Basics/src/hmr/tutorial/wordcount/WordCount.java
+++ Basics/src/hmr/tutorial/wordcount/WordCount.java
@@ -14,12 +14,16 @@ import java.io.IOException;
 
 public class WordCount {
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        if (args.length != 2) {
+            System.out.println(" usage: arg[0] is input path, arg[1] is output path");
+            return;
+        }
 
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
 
         FileSystem fs = FileSystem.get(conf);
-        fs.delete(new Path("/user/wangboo/hadoop-demo/wordcount/output/"), true);
+        fs.delete(new Path(args[1]), true);
 
         job.setJarByClass(WordCount.class);
         job.setMapperClass(WordCountMapper.class);
